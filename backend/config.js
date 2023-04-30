e496aee26d15df68bd9308a791f5bf7c4218dc16
baspop01 <73678928+baspop01@users.noreply.github.com>
2023-04-30 18:41:39 +0700

    Add files via upload

--- backend/config.js
+++ backend/config.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'CMBrfq45',
+  host: 'mysql.cykgenz62nm7.us-east-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'Pang123456',
+  port: '3306',
   database: 'project_database',
   waitForConnections: true,
   connectionLimit: 10,
